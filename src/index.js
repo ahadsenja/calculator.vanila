c5e13c553b8bf8d21089a19902eb5b978dbeb198
ahadsenja <hamdanilm96@gmail.com>
2023-04-23 06:15:17 +0800

    initial calculator app with vanila js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function onClickButtonPlus() {
+    const testing = document.querySelector(".plus")
+    console.log(testing)
+}
+
+function onClickButtonMin() {
+    const testing = document.querySelector(".min")
+    console.log(testing)
+}
+
+function onClickButtonMul() {
+    const testing = document.querySelector(".mul")
+    console.log(testing)
+}
+
+function onClickButtonDiv() {
+    const testing = document.querySelector(".div")
+    console.log(testing)
+}
+
+function onClickButtonMod() {
+    const testing = document.querySelector(".mod")
+    console.log(testing)
+}
+
+function onClickButtonEql() {
+    const testing = document.querySelector(".eql")
+    console.log(testing)
+}
